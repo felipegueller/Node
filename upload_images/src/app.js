ccb26e3a76c700381dbff4214e5f736efc266873
felipegueller <felipe.gueller@gmail.com>
2022-08-12 11:48:45 -0300

    :sparkles: feat: using the multer middleware on route

--- upload_images/src/app.js
+++ upload_images/src/app.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const app = express()
+const uploadUser = require('../middlewares/upload-images')
 
-app.get('/upload-image', async (req, res) => {
+app.get('/upload-image', uploadUser.single('image') , async (req, res) => {
+  if (req.file) {
+    console.log('imagem: ', req.file)
+    return res.json({
+      error: false,
+      message: 'Upload was realized with sucess!'
+    })
+  }
+
   return res.status(400).json({
     error: true,
     message: 'The upload had an error!'
   })
-
-  // return res.json({
-  //   error: false,
-  //   message: 'Upload was realized with sucess!'
-  // })
 })
 
 app.listen(3030, () => console.log('The server is running on port 3030'))
